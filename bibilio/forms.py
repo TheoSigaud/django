eef56a87fefbf9ce957dd62531f196dff738e5a9
Samy Hamed-e-saberi <samy.saberi@gmail.com>
2022-11-18 15:32:10 +0100

    custom authenfication

--- bibilio/forms.py
+++ bibilio/forms.py
@@ -1,5 +1,8 @@
 from django.forms import ModelForm
 from bibilio.models import Book, Author, Gender, Editor
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from bibilio.models import User
 
 class AuthorForm(ModelForm):
     class Meta:
@@ -20,3 +23,17 @@ class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'editor', 'gender', 'jacket']
+
+
+
+class RegisterForm(UserCreationForm):
+  first_name = forms.CharField(label='First name', max_length=100)
+  last_name = forms.CharField(label='Last name', max_length=100)
+  email = forms.EmailField(label='Email', max_length=100)
+  adress = forms.CharField(label='Adress', max_length=100)
+  city = forms.CharField(label='City', max_length=100)
+  zipCode = forms.CharField(label='Zip code', max_length=100)
+
+  class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'adress', 'city', 'zipCode', 'password1', 'password2')
