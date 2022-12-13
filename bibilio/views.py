e6d3bec9cb6fd8ab0c9f932b1940da18c2eb4929
Samy Hamed-e-saberi <samy.saberi@gmail.com>
2022-12-13 18:32:41 +0100

    auth libraire

--- bibilio/views.py
+++ bibilio/views.py
@@ -44,7 +44,7 @@ def home(request):
     if request.user.is_authenticated:
         return render(request, 'bibilio/home.html')
     else:
-        return redirect('/login')
+        return redirect('/')
 
 def signin(request):
     if request.user.is_authenticated:
